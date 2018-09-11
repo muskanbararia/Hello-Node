ae73657173a51a79cede1f89a322f8ad9050d066
Muskan Bararia <muskanbararia@gmail.com>
2018-09-11 10:04:42 +0530

    Update hello-file.js

--- hello-file/hello-file.js
+++ hello-file/hello-file.js
@@ -1,12 +1,13 @@
 //creating an object of http module
-const http = require('http');
+var http = require('http');
+// creating an object of fs module
 var fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var hostname = '127.0.0.1';
+var port = 3000;
 
 //Creating server object to return the url
-const server = http.createServer((req, res) => {
+var server = http.createServer((req, res) => {
 	//reading data from html file
     fs.readFile('one.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
